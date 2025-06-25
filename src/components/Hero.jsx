e6c855920d56ca984a64e986530634ec08320875
menakumari1 <menakri8252@gmail.com>
2025-06-25 19:24:12 +0530

    Implement Hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,13 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+ const videoRef = useRef();
  
+ const isMobile = useMediaQuery({ maxWidth: 767 });
  
  useGSAP(() => {
 	const heroSplit = new SplitText(".title", {
@@ -33,7 +37,7 @@ const Hero = () => {
 	 delay: 1,
 	});
 	
-	gsap 
+	gsap
 	.timeline({
 	 scrollTrigger: {
 		trigger: "#hero",
@@ -46,8 +50,24 @@ const Hero = () => {
 	.to(".left-leaf", { y: -200 }, 0)
 	.to(".arrow", { y: 100 }, 0);
 	
+	const startValue = isMobile ? "top 50%" : "center 60%";
+	const endValue = isMobile ? "120% top" : "bottom top";
 	
+	let tl = gsap.timeline({
+	 scrollTrigger: {
+		trigger: "video",
+		start: startValue,
+		end: endValue,
+		scrub: true,
+		pin: true,
+	 },
+	});
 	
+	videoRef.current.onloadedmetadata = () => {
+	 tl.to(videoRef.current, {
+		currentTime: videoRef.current.duration,
+	 });
+	};
  }, []);
  
  return (
@@ -67,7 +87,7 @@ const Hero = () => {
 		/>
 		
 		<div className="body">
-		 {/* <img src="/images/arrow.png" alt="arrow" className="arrow" /> */}
+		 {/* <img src="/images/arSrow.png" alt="arrow" className="arrow" /> */}
 		 
 		 <div className="content">
 			<div className="space-y-5 hidden md:block">
@@ -83,12 +103,21 @@ const Hero = () => {
 				creative flair, and timeless recipes — designed to delight your
 				senses.
 			 </p>
-			 <a href="#cocktails">View cocktails</a>
+			 <a href="#cocktails" >View cocktails</a>
 			</div>
 		 </div>
 		</div>
 	 </section>
 	 
+	 <div className="video absolute inset-0">
+		<video
+		 ref={videoRef}
+		 muted
+		 playsInline
+		 preload="auto"
+		 src="/videos/output.mp4"
+		/>
+	 </div>
 	</>
  );
 };
